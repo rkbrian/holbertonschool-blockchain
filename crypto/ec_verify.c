fb45bfd3c9ba62f5dec863dee78fa5d6ea0f689e
rkbrian <rkbriankong@gmail.com>
2022-03-25 23:51:05 +0000

    7 test

---
+++ crypto/ec_verify.c
@@ -0,0 +1,23 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_verify - verify the signature of a given set of bytes, using a given
+ *  EC_KEY public key.
+ * @key: points to the EC_KEY structure containing the public key to be used
+ *  to verify the signature
+ * @msg: points to the msglen characters to verify the signature of
+ * @msglen: msg length
+ * @sig: points to the signature to be checked
+ * Return: 1 if the signature is valid, and 0 otherwise
+ */
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+	      sig_t const *sig)
+{
+	int validity;
+
+	if (!key || !msg || !sig)
+		return (0);
+	validity = ECDSA_verify(0, msg, (int)msglen,
+				(unsigned char *)sig->sig, (int)sig->len, (EC_KEY *)key);
+	return (validity);
+}
