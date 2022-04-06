79f344e81b82056f792cd1b4b665216b7cdd2d5a
rkbrian <rkbriankong@gmail.com>
2022-04-06 17:18:45 +0000

    prep for 5

---
+++ blockchain/v0.1/blockchain_serialize.c
@@ -0,0 +1,13 @@
+#include "blockchain.h"
+
+/**
+ * blockchain_serialize - contain the path to a file to serialize
+ *  the Blockchain into
+ * @blockchain: points to the Blockchain to be serialized
+ * @path: points to an existing file, it must be overwritten
+ * Return: 0 upon success, or -1 upon failure
+ */
+int blockchain_serialize(blockchain_t const *blockchain, char const *path)
+{
+	;
+}
