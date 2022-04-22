9085bc888e59a002b7fbcdd7a6425c3fa4e1b115
rkbrian <rkbriankong@gmail.com>
2022-04-22 19:41:50 +0000

    block mine: betty

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -5,13 +5,13 @@
  *  must find a hash for block that matches its difficulty
  * @block: points to the Block to be mined
  */
-void block_mine(block_t *block);
+void block_mine(block_t *block)
 {
 	if (!block)
 		return;
-	if (block->ino.difficulty == 0)
+	if (block->info.difficulty == 0)
 		return;
 	block_hash(block, block->hash);
-	while (!hash_matches_difficulty(hash, difficulty))
+	while (!hash_matches_difficulty(block->hash, block->info.difficulty))
 		block->info.nonce += 1, block_hash(block, block->hash);
 }
