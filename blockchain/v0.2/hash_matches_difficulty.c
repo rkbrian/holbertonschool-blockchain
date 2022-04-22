cc89742b5875c20409d9a6a20d8fd937dad6093a
rkbrian <rkbriankong@gmail.com>
2022-04-22 05:54:59 +0000

    test 0

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -9,5 +9,22 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 			    uint32_t difficulty)
 {
-	;
+	uint32_t i, binary_ha, test_diff = 0;
+
+	for (i = 0; i < SHA256_DIGEST_LENGTH && test_diff < difficulty;
+	     test_diff += 8, i++) /* leap of bite size = 8 */
+	{
+		if (128 & hash[i])
+			return (0);
+	}
+	test_diff -= 8, i--;
+	for (binary_ha = 128; binary_ha > 0; binary_ha = binary_ha >> 1)
+	{
+		if (binary_ha & hash[i])
+			break;
+		test_diff++;
+	}
+	if (test_diff >= difficulty)
+		return (1);
+	return (0);
 }
