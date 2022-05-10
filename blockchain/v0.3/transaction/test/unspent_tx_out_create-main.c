0ca20d78ed4bb00ecc256da28dbdf0ca08664188
rkbrian <rkbriankong@gmail.com>
2022-05-10 15:47:00 +0000

    unspent tx out create

--- blockchain/v0.3/transaction/test/unspent_tx_out_create-main.c
+++ blockchain/v0.3/transaction/test/unspent_tx_out_create-main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "blockchain.h"
+#include "transaction.h"
 
 void _print_hex_buffer(uint8_t const *buf, size_t len);
 
--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,7 +1,8 @@
 #include "transaction.h"
 
 /**
- * tx_out_create - allocate and initialize a transaction output structure
+ * unspent_tx_out_create - allocate and initialize an unspent transaction
+ *  output structure
  * @block_hash: the hash of the Block where the referenced transaction output
  *  is located
  * @tx_id: the hash of a transaction in the Block block_hash, where the
@@ -10,9 +11,17 @@
  * Return: a pointer to the created unspent transaction output upon success,
  *  or NULL upon failure
  */
-unspent_tx_out_t *unspent_tx_out_create(
-	uint8_t block_hash[SHA256_DIGEST_LENGTH],uint8_t tx_id[SHA256_DIGEST_LENGTH],
-	tx_out_t const *out)
+unspent_tx_out_t *unspent_tx_out_create(uint8_t
+					block_hash[SHA256_DIGEST_LENGTH],
+					uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
 {
-	;
+	unspent_tx_out_t *uns_out;
+
+	uns_out = malloc(sizeof(unspent_tx_out_t));
+	if (!uns_out)
+		return (NULL);
+	memcpy(uns_out->block_hash, block_hash, sizeof(uns_out->block_hash));
+	memcpy(uns_out->tx_id, tx_id, sizeof(uns_out->tx_id));
+	memcpy(&uns_out->out, out, sizeof(uns_out->out));
+	return (uns_out);
 }
