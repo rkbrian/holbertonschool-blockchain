e3cb63445bfecec2ff1cbcfae1c2ac250549e132
rkbrian <rkbriankong@gmail.com>
2022-05-12 16:42:55 +0000

    sig

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -20,5 +20,7 @@ tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 	memcpy(in->block_hash, unspent->block_hash, sizeof(in->block_hash));
 	memcpy(in->tx_id, unspent->tx_id, sizeof(in->tx_id));
 	memcpy(in->tx_out_hash, unspent->out.hash, sizeof(in->tx_out_hash));
+	memset(&in->sig.sig, 0, sizeof(in->sig.sig));
+	in->sig.len = 0;
 	return (in);
 }
