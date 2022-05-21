2625fb99c01deb97b9cd5f2ae5a1f205cb374175
rkbrian <rkbriankong@gmail.com>
2022-05-21 17:24:42 +0000

    free transaction

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -94,15 +94,12 @@ int transaction_is_valid(transaction_t const *transaction,
 			 llist_t *all_unspent);
 transaction_t *coinbase_create(EC_KEY const *receiver, uint32_t block_index);
 int coinbase_is_valid(transaction_t const *coinbase, uint32_t block_index);
-
-
+void transaction_destroy(transaction_t *transaction);
 
 /* helper functions */
 transaction_t *tx_init(void);
 uint32_t input_selector(llist_t *input_list, llist_t *all_unspent,
 			uint32_t amount, uint8_t key_in[EC_PUB_LEN]);
 int zeroed_mem(void *ptr, size_t size);
-
-
 
 #endif /* TRANSACTION_H */
---
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -0,0 +1,17 @@
+#include "blockchain.h"
+
+/**
+ * transaction_destroy - function that deallocates a transaction structure
+ * @transaction: points to the transaction to delete
+ */
+void transaction_destroy(transaction_t *transaction)
+{
+	if (transaction)
+	{
+		if (transaction->inputs)
+			llist_destroy(transaction->inputs, 1, free);
+		if (transaction->outputs)
+			llist_destroy(transaction->outputs, 1, free);
+		free(transaction);
+	}
+}
