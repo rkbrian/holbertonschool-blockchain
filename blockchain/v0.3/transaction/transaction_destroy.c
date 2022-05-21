e60f482c2904a70507a16b89108f1d0851c32183
rkbrian <rkbriankong@gmail.com>
2022-05-21 19:36:15 +0000

    block and blockchain transaction init and destroy

--- blockchain/v0.3/transaction/transaction_destroy.c
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -9,9 +9,9 @@ void transaction_destroy(transaction_t *transaction)
 	if (transaction)
 	{
 		if (transaction->inputs)
-			llist_destroy(transaction->inputs, 1, free);
+			llist_destroy(transaction->inputs, 1, NULL);
 		if (transaction->outputs)
-			llist_destroy(transaction->outputs, 1, free);
+			llist_destroy(transaction->outputs, 1, NULL);
 		free(transaction);
 	}
 }
