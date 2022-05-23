0d306035f41520b6f6037b0a2ed240ea467acb28
rkbrian <rkbriankong@gmail.com>
2022-05-23 04:07:33 +0000

    version change

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -12,7 +12,7 @@
 #define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\
 \x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 #define HBLK_MAGIC "HBLK"
-#define HBLK_VERSION "0.1"
+#define HBLK_VERSION "0.2"
 /* Defines how often (in seconds) a Block should be found: */
 #define BLOCK_GENERATION_INTERVAL 1
 /* Defines how often (in Blocks) the difficulty should be adjusted: */
