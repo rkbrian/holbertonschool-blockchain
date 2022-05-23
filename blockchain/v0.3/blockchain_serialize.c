a218fddb37836ec7bde71320322bffb6beacec78
rkbrian <rkbriankong@gmail.com>
2022-05-23 10:25:20 +0000

    serialization and deserialization updates

--- blockchain/v0.3/blockchain_serialize.c
+++ blockchain/v0.3/blockchain_serialize.c
@@ -66,24 +66,24 @@ void tx_serialize(llist_t *transactions, FILE *fp, int endianness)
 	{
 		tx = llist_get_node_at(transactions, i);
 		nb_inputs = llist_size(tx->inputs), nb_outputs = llist_size(tx->outputs);
-		block_swap(&tx->id, sizeof(tx->id), fp, endi);
-		block_swap(&nb_inputs, sizeof(nb_inputs), fp, endi);
-		block_swap(&nb_outputs, sizeof(nb_outputs), fp, endi);
+		block_swap(&tx->id, sizeof(tx->id), fp, endianness);
+		block_swap(&nb_inputs, sizeof(nb_inputs), fp, endianness);
+		block_swap(&nb_outputs, sizeof(nb_outputs), fp, endianness);
 		for (j = 0; j < llist_size(tx->inputs); j++)
 		{
 			tx_in = llist_get_node_at(tx->inputs, j);
-			block_swap(&tx_in->block_hash, sizeof(tx_in->block_hash), fp, endi);
-			block_swap(&tx_in->tx_id, sizeof(tx_in->tx_id), fp, endi);
-			block_swap(&tx_in->tx_out_hash, sizeof(tx_in->tx_out_hash), fp, endi);
-			block_swap(&tx_in->sig.sig, sizeof(tx_in->sig.sig), fp, endi);
-			block_swap(&tx_in->sig.len, sizeof(tx_in->sig.len), fp, endi);
+			block_swap(&tx_in->block_hash, sizeof(tx_in->block_hash), fp, endianness);
+			block_swap(&tx_in->tx_id, sizeof(tx_in->tx_id), fp, endianness);
+			block_swap(&tx_in->tx_out_hash, sizeof(tx_in->tx_out_hash), fp, endianness);
+			block_swap(&tx_in->sig.sig, sizeof(tx_in->sig.sig), fp, endianness);
+			block_swap(&tx_in->sig.len, sizeof(tx_in->sig.len), fp, endianness);
 		}
 		for (j = 0; j < llist_size(tx->outputs); j++)
 		{
 			tx_out = llist_get_node_at(tx->outputs, j);
-			block_swap(&tx_out->amount, sizeof(tx_out->amount), fp, endi);
-			block_swap(&tx_out->pub, sizeof(tx_out->pub), fp, endi);
-			block_swap(&tx_out->hash, sizeof(tx_out->hash), fp, endi);
+			block_swap(&tx_out->amount, sizeof(tx_out->amount), fp, endianness);
+			block_swap(&tx_out->pub, sizeof(tx_out->pub), fp, endianness);
+			block_swap(&tx_out->hash, sizeof(tx_out->hash), fp, endianness);
 		}
 	}
 }
@@ -102,11 +102,11 @@ void uns_serialize(llist_t *unspent, FILE *fp, int endianness)
 	for (i = 0; i < llist_size(unspent); i++)
 	{
 		uns = llist_get_node_at(unspent, i);
-		block_swap(&uns->block_hash, sizeof(uns->block_hash), fp, endi);
-		block_swap(&uns->tx_id, sizeof(uns->tx_id), fp, endi);
-		block_swap(&uns->out.amount, sizeof(uns->out.amount), fp, endi);
-		block_swap(&uns->out.pub, sizeof(uns->out.pub), fp, endi);
-		block_swap(&uns->out.hash, sizeof(uns->out.hash), fp, endi);
+		block_swap(&uns->block_hash, sizeof(uns->block_hash), fp, endianness);
+		block_swap(&uns->tx_id, sizeof(uns->tx_id), fp, endianness);
+		block_swap(&uns->out.amount, sizeof(uns->out.amount), fp, endianness);
+		block_swap(&uns->out.pub, sizeof(uns->out.pub), fp, endianness);
+		block_swap(&uns->out.hash, sizeof(uns->out.hash), fp, endianness);
 	}
 }
 
